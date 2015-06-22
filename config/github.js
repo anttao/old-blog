2b47a45ab4384803fecebe59f9568a478fd5ec1c
yanjixiong <yjk99@qq.com>
2015-06-22 19:23:04 +0800

    Update github.js

--- config/github.js
+++ config/github.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "clientId" : "b9638043186efd082292",
-    "clientSecret" : "6c9f989f64e40e19a2e2fd5566aecbcca48256e9",
+    "clientId" : "xxxx",
+    "clientSecret" : "xxxx",
     "callbackURL" : "/login/github/callback"
 };
